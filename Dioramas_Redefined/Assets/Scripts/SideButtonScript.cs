29a26edbfdffb351bcaf9f94cec95df535d5be6f
Nathaniel Gomez <gomez365@umn.edu>
2019-05-01 12:55:03 -0500

    Created UI for viz view, added script to change views on button press

---
+++ Dioramas_Redefined/Assets/Scripts/SideButtonScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideButtonScript : MonoBehaviour
+{
+
+    public GameObject DioPanel;
+    public GameObject VizPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeView(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeView(int choice)
+    {
+        switch (choice)
+        {
+            case 0:
+                DioPanel.SetActive(true);
+                VizPanel.SetActive(false);
+                break;
+            case 1:
+                VizPanel.SetActive(true);
+                DioPanel.SetActive(false);
+                break;
+            default:
+                break;
+
+        }
+    }
+}
