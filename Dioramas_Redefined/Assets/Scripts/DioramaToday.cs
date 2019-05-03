532042caa0dab56e23f8f646c79a039cad655c42
Thomas Blackwell <twblackwell@gmail.com>
2019-05-03 16:14:34 -0500

    added buttons between 360 Image and NowMenu scenes

---
+++ Dioramas_Redefined/Assets/Scripts/DioramaToday.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DioramaToday : MonoBehaviour
+{
+	public Button button360;
+	
+	void Start() {
+		button360.onClick.AddListener(() => {
+			SceneManager.LoadScene("Diorama Today", LoadSceneMode.Single);
+		});
+	}
+}
---
+++ Dioramas_Redefined/Assets/Scripts/ImagetoMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ImagetoMenu : MonoBehaviour
+{
+	public Button buttonMenu;
+	
+	void Start() {
+		buttonMenu.onClick.AddListener(() => {
+			SceneManager.LoadScene("NowMenuScene", LoadSceneMode.Single);
+		});
+	}
+}
